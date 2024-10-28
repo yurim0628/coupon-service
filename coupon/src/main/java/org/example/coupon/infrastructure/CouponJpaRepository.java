985b7f8cba420380f1e64ac8eb828c6f95eb089b
yurim0628 <dbfla0303@naver.com>
2024-10-28 23:01:33 +0900

    feat: 데이터베이스 접근을 위한 CouponJpaRepository 추가

---
+++ coupon/src/main/java/org/example/coupon/infrastructure/CouponJpaRepository.java
@@ -0,0 +1,14 @@
+package org.example.coupon.infrastructure;
+
+import io.lettuce.core.dynamic.annotation.Param;
+import org.example.coupon.infrastructure.entity.CouponEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CouponJpaRepository extends JpaRepository<CouponEntity, Long> {
+
+    @Modifying
+    @Query("update CouponEntity c set c.issuedQuantity = :issuedQuantity where c.id = :id")
+    void updateIssuedQuantity(@Param("id") Long id, @Param("issuedQuantity") Long issuedQuantity);
+}
