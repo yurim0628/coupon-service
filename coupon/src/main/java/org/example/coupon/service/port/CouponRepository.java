84c6c2eb24227b8b731b868fe7bca333ca5675cc
yurim0628 <dbfla0303@naver.com>
2024-10-28 22:58:27 +0900

    feat: 쿠폰 데이터 관리를 위한 CouponRepository 인터페이스 추가

---
+++ coupon/src/main/java/org/example/coupon/service/port/CouponRepository.java
@@ -0,0 +1,15 @@
+package org.example.coupon.service.port;
+
+import org.example.coupon.domain.Coupon;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CouponRepository {
+
+    Optional<Coupon> findById(Long id);
+
+    List<Coupon> findAll();
+
+    void updateIssuedQuantity(Long id, Long issuedQuantity);
+}
