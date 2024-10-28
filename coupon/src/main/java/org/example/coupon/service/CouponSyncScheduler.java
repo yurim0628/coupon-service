b0ec60aa4192b31631a928da3de5a7b928ce3ec6
yurim0628 <dbfla0303@naver.com>
2024-10-28 23:02:39 +0900

    feat: Redis와 데이터베이스의 쿠폰 발급 수량 동기화를 위한 CouponSyncScheduler 구현

---
+++ coupon/src/main/java/org/example/coupon/service/CouponSyncScheduler.java
@@ -0,0 +1,37 @@
+package org.example.coupon.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.coupon.domain.Coupon;
+import org.example.coupon.service.port.CouponRepository;
+import org.example.redis.service.CouponIssueRedisService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CouponSyncScheduler {
+
+    private static final Long NO_ISSUED_COUNT = 0L;
+    private final CouponService couponService;
+    private final CouponIssueRedisService couponIssueRedisService;
+    private final CouponRepository couponRepository;
+
+    @Scheduled(initialDelay = 60000, fixedDelay = 60000)
+    @Transactional
+    public void issuedQuantityUpdate() {
+        List<Coupon> coupons = couponService.getCoupons();
+        for (Coupon coupon : coupons) {
+            Long couponId = coupon.id();
+            Long issuedCount = couponIssueRedisService.getIssuedCouponCount(couponId);
+            if (!Objects.equals(issuedCount, NO_ISSUED_COUNT)) {
+                couponRepository.updateIssuedQuantity(couponId, issuedCount);
+            }
+        }
+    }
+}
