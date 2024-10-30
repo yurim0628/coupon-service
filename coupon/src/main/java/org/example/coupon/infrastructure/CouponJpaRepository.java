17570b1d3a9ee35423f652d521c6dcdd1000b03c
yurim0628 <dbfla0303@naver.com>
2024-10-30 16:22:14 +0900

    feat: CouponJpaRepository에 findCouponsByEventId 메서드 추가

--- coupon/src/main/java/org/example/coupon/infrastructure/CouponJpaRepository.java
+++ coupon/src/main/java/org/example/coupon/infrastructure/CouponJpaRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface CouponJpaRepository extends JpaRepository<CouponEntity, Long> {
 
     @Modifying
     @Query("update CouponEntity c set c.issuedQuantity = :issuedQuantity where c.id = :id")
     void updateIssuedQuantity(@Param("id") Long id, @Param("issuedQuantity") Long issuedQuantity);
+
+    List<CouponEntity> findCouponsByEventId(Long eventId);
 }
