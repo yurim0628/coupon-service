44579be929ab9c2a81aecbbe636fbe75dc8c9d5a
yurim0628 <dbfla0303@naver.com>
2024-10-30 16:22:42 +0900

    feat: CouponService에 이벤트 ID로 쿠폰 조회 기능 추가

--- coupon/src/main/java/org/example/coupon/service/CouponService.java
+++ coupon/src/main/java/org/example/coupon/service/CouponService.java
@@ -3,10 +3,12 @@ package org.example.coupon.service;
 import lombok.RequiredArgsConstructor;
 import org.example.common.exception.CommonException;
 import org.example.coupon.domain.Coupon;
+import org.example.coupon.domain.CouponResponse;
 import org.example.coupon.service.port.CouponRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.example.common.exception.ErrorCode.COUPON_NOT_FOUND;
 
@@ -16,12 +18,19 @@ public class CouponService {
 
     private final CouponRepository couponRepository;
 
+    public List<Coupon> getAllCoupons() {
+        return couponRepository.findAll();
+    }
+
+    public List<CouponResponse> getCoupons(Long eventId) {
+        List<Coupon> coupons = couponRepository.findCouponsByEventId(eventId);
+        return coupons.stream()
+                .map(CouponResponse::of)
+                .collect(Collectors.toList());
+    }
+
     public Coupon getCoupon(Long couponId) {
         return couponRepository.findById(couponId)
                 .orElseThrow(() -> new CommonException(COUPON_NOT_FOUND));
-    }
-
-    public List<Coupon> getCoupons() {
-        return couponRepository.findAll();
     }
 }
