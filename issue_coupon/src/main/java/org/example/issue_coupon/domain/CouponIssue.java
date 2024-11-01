7c5bb40272187353a56b6b0eda98cbeb52ab4869
yurim0628 <dbfla0303@naver.com>
2024-11-01 12:24:19 +0900

    refactor: CouponIssue 도메인에 사용자 ID 필드 추가하여 객체 생성

--- issue_coupon/src/main/java/org/example/issue_coupon/domain/CouponIssue.java
+++ issue_coupon/src/main/java/org/example/issue_coupon/domain/CouponIssue.java
@@ -10,18 +10,21 @@ public class CouponIssue {
     private final Long id;
     private final CouponStatus couponStatus;
     private final Long couponId;
+    private final Long userId;
 
     @Builder
-    private CouponIssue(Long id, CouponStatus couponStatus, Long couponId) {
+    private CouponIssue(Long id, CouponStatus couponStatus, Long couponId, Long userId) {
         this.id = id;
         this.couponStatus = couponStatus;
         this.couponId = couponId;
+        this.userId = userId;
     }
 
     public static CouponIssue from(CouponIssueCreate couponIssueCreate) {
         return CouponIssue.builder()
                 .couponStatus(ACTIVE)
                 .couponId(couponIssueCreate.couponId())
+                .userId(couponIssueCreate.userId())
                 .build();
     }
 }
