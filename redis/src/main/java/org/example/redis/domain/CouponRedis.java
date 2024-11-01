a0763337d672e9bd0d30d91cdd3ed12fe716634c
yurim0628 <dbfla0303@naver.com>
2024-11-01 12:29:15 +0900

    refactor: CouponRedis의 of 메서드에 생성자 직접 사용으로 객체 생성 방식 단순화

--- redis/src/main/java/org/example/redis/domain/CouponRedis.java
+++ redis/src/main/java/org/example/redis/domain/CouponRedis.java
@@ -1,11 +1,15 @@
 package org.example.redis.domain;
 
-import lombok.Builder;
-
-@Builder
 public record CouponRedis(
         Long id,
         Long maxQuantity,
         Long eventId
 ) {
+    public static CouponRedis of(CouponRedisCreate couponRedisCreate) {
+        return new CouponRedis(
+                couponRedisCreate.id(),
+                couponRedisCreate.maxQuantity(),
+                couponRedisCreate.eventId()
+        );
+    }
 }
